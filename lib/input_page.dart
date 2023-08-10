1d576bb78e3c6b63f46f01be5ae2089c8c351d9c
Guilherme Costa <guilhermecosta@MacBook-Pro-de-Guilherme.local>
2023-08-09 22:37:10 -0300

    Add result page

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -3,6 +3,8 @@ import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'icon_content.dart';
 import 'reusable_card.dart';
 import 'constants.dart';
+import 'results_page.dart';
+import 'bottom_button.dart';
 
 enum Gender {
   male,
@@ -130,14 +132,19 @@ class _InputPageState extends State<InputPage> {
               ],
             ),
           ),
-          Container(
-            color: kBottomContainerColour,
-            margin: const EdgeInsets.only(top: 10.0),
-            width: double.infinity,
-            height: kBottomContainerHeight,
+          BottomButton(
+            title: 'CALCULATE',
+            onTap: () {
+              Navigator.push(context, 
+                MaterialPageRoute(
+                  builder: (context) => ResultsPage(),
+                ),
+              );
+            }
           ),
         ],
       ),
     );
   }
 }
+
---
+++ lib/results_page.dart
@@ -0,0 +1,53 @@
+import 'package:bmi_calculator_flutter/constants.dart';
+import 'package:bmi_calculator_flutter/reusable_card.dart';
+import 'package:flutter/material.dart';
+
+class ResultsPage extends StatelessWidget {
+  const ResultsPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('BMI CALCULATOR'),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Expanded(
+            child: Text(
+              'Your Result',
+              style: kTitleTextStyle,
+            ),
+          ),
+          Expanded(
+            flex: 5,
+            child: ReusableCard(
+              colour: kActiveCardColour,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Text(
+                    'Normal',
+                    style: kResultTextStyle,
+                  ),
+                  Text(
+                    '18.3',
+                    style: kBMITextStyle,
+                  ),
+                  Text(
+                    'Your BMI result is quite low, you should eat more.',
+                    textAlign: TextAlign.center,
+                    style: kBodyTextStyle,
+                  )
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
